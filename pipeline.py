0420a86f25442cc0cf8ac193bc23503b694e3249
mohaubai <mubaid675@gmail.com>
2026-08-13 20:48:26 +0000

    Removed the comments from the actual code

--- pipeline.py
+++ pipeline.py
@@ -19,13 +19,13 @@ def process_data():
     csv_buffer = StringIO()
     df.to_csv(csv_buffer, index=False)
 
-    # s3 = boto3.client('s3')
-    # bucket_name = 'my-data-lab-august-2026-ub'  # <-- You will change this
-    # s3.put_object(
-    #     Bucket=bucket_name,
-    #     Key='clean/new_output.csv',
-    #     Body=csv_buffer.getvalue()
-    # )
+    s3 = boto3.client('s3')
+    bucket_name = 'my-data-lab-august-2026-ub'  # <-- You will change this
+    s3.put_object(
+        Bucket=bucket_name,
+        Key='clean/new_output.csv',
+        Body=csv_buffer.getvalue()
+    )
 
     print(f"Uploaded {len(df)} rows to s3://{bucket_name}/clean/new_output.csv")
     print(df)
