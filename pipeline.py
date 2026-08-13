ea7205fe2b537f3934b5187e3a9ec9623b66f30a
mohaubai <mubaid675@gmail.com>
2026-08-13 20:46:31 +0000

    Fix KeyError by Stripping column spaces

--- pipeline.py
+++ pipeline.py
@@ -11,20 +11,21 @@ def process_data():
     4,Diana, 28, Houston
     5,Eve, 32, Phoenix
     """
-
+    
     df = pd.read_csv(StringIO(csv_data))
+    df.columns = df.columns.str.strip()
     df['age_group'] = df['age'].apply(lambda x: 'young' if x < 30 else 'adult')
 
     csv_buffer = StringIO()
     df.to_csv(csv_buffer, index=False)
 
-    s3 = boto3.client('s3')
-    bucket_name = 'my-data-lab-august-2026-ub'  # <-- You will change this
-    s3.put_object(
-        Bucket=bucket_name,
-        Key='clean/new_output.csv',
-        Body=csv_buffer.getvalue()
-    )
+    # s3 = boto3.client('s3')
+    # bucket_name = 'my-data-lab-august-2026-ub'  # <-- You will change this
+    # s3.put_object(
+    #     Bucket=bucket_name,
+    #     Key='clean/new_output.csv',
+    #     Body=csv_buffer.getvalue()
+    # )
 
     print(f"Uploaded {len(df)} rows to s3://{bucket_name}/clean/new_output.csv")
     print(df)
